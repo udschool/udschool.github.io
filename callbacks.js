43a0e294bce6c1a35c730246cc08c71492864211
sk8beemo <skateor2007@gmail.com>
2020-08-20 17:13:14 +0300

    add API func

--- callbacks.js
+++ callbacks.js
@@ -1,3 +1,4 @@
+// callbacks
 function updateSortVis(value) {
 	var urlElse = window.location.href.split('&vis')[0];
 	var urlIf = window.location.href.split('?vis')[0];
@@ -45,63 +46,7 @@ function putProductId (product_id) {
 }
 
 function printProductInfo(product_id) {
-	var productInfo = ['<div id="my-store-35020171"></div><div>'];
-	products.forEach(function(entry){
-		let id = entry.id;
-		if (id == product_id) {
-			productInfo.push(`<div id="my-store-35020171"></div><div><script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?35020171&data_platform=code&data_date=2020-08-18" charset="utf-8">${"</sc"+"ript>"}<script type="text/javascript">window.ec=window.ec||{}; window.ec.storefront=window.ec.storefront||{}; window.ec.storefront.enable_navigation=true;window.ec.storefront.product_details_layout="TWO_COLUMNS_SIDEBAR_ON_THE_RIGHT";window.ec.storefront.product_details_gallery_layout="SINGLE_IMAGE";window.ec.storefront.product_details_two_columns_with_right_sidebar_show_product_description_on_sidebar=true;window.ec.storefront.product_details_two_columns_with_left_sidebar_show_product_description_on_sidebar=false;window.ec.storefront.product_details_show_product_name=true;window.ec.storefront.product_details_show_breadcrumbs=false;window.ec.storefront.product_details_show_product_sku=true;window.ec.storefront.product_details_show_product_price=true;window.ec.storefront.product_details_show_in_stock_label=false;window.ec.storefront.product_details_show_number_of_items_in_stock=false;window.ec.storefront.product_details_show_qty=false;window.ec.storefront.product_details_show_wholesale_prices=false;window.ec.storefront.product_details_show_product_options=true;window.ec.storefront.product_details_show_product_description=true;window.ec.storefront.product_details_show_share_buttons=true;window.ec.storefront.product_details_position_product_name=100;window.ec.storefront.product_details_position_breadcrumbs=200;window.ec.storefront.product_details_position_product_sku=300;window.ec.storefront.product_details_position_product_price=400;window.ec.storefront.product_details_position_product_options=500;window.ec.storefront.product_details_position_buy_button=600;window.ec.storefront.product_details_position_wholesale_prices=700;window.ec.storefront.product_details_position_product_description=800;window.ec.storefront.product_details_position_share_buttons=900;xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","defaultProductId=${product_id}","id=my-store-35020171");${"</sc"+"ript>"}</div>`);
-		}
-	});
-	$('#t').html(productInfo.join(""));
-}
-
-function printProductsFromCategories (category, callback) {
-	var productsSortLayout = [];
-	products.forEach(function(entry) {
-		let id = entry.id;
-		if (entry.category == category) {
-			productsSortLayout.push(`<div class="item ecsp ecsp-SingleProduct-v2 ecsp-SingleProduct-v2-bordered ecsp-Product ec-Product-${id}" itemscope itemtype="http://schema.org/Product" data-single-product-id="${id}"> <a onclick="putProductId('${id}')"> <div itemprop="image"></div><div class="ecsp-title" itemprop="name" content="ПУФ"></div><div itemtype="http://schema.org/Offer" itemscope itemprop="offers"> </a> <div class="ecsp-productBrowser-price ecsp-price" itemprop="price" content="1"> <div itemprop="priceCurrency" content="RUB"></div></div></div><div customprop="options"></div><div customprop="addtobag"></div></div>`);
-		}
-	});
-	callback(productsSortLayout);
-	$('#t').html(productsSortLayout.join(""));
-}
-
-function printProductsVisFromCategories (vis, category, callback) {
-	console.log(vis + ' ' + category);
-	var productsVisSortLayout = [];
-	products.forEach(function(entry) {
-		let id = entry.id;
-		if (entry.category == category) {
-			if (entry.vis == vis) {
-				productsVisSortLayout.push(`<div class="item ecsp ecsp-SingleProduct-v2 ecsp-SingleProduct-v2-bordered ecsp-Product ec-Product-${id}" itemscope itemtype="http://schema.org/Product" data-single-product-id="${id}"> <a onclick="putProductId('${id}')"> <div itemprop="image"></div><div class="ecsp-title" itemprop="name" content="ПУФ"></div><div itemtype="http://schema.org/Offer" itemscope itemprop="offers"> </a> <div class="ecsp-productBrowser-price ecsp-price" itemprop="price" content="1"> <div itemprop="priceCurrency" content="RUB"></div></div></div><div customprop="options"></div><div customprop="addtobag"></div></div>`);
-			}
-		}
-	});
-	callback(productsVisSortLayout);
-	$('#t').html(productsVisSortLayout.join(""));
-}
-
-function printProductsVis (vis_option, callback) {
-	var productsVisLayout = [];
-	products.forEach(function (entry) {
-		let id = entry.id;
-		if (entry.vis == vis_option) {
-			productsVisLayout.push(`<div class="item ecsp ecsp-SingleProduct-v2 ecsp-SingleProduct-v2-bordered ecsp-Product ec-Product-${id}" itemscope itemtype="http://schema.org/Product" data-single-product-id="${id}"> <a onclick="putProductId('${id}')"> <div itemprop="image"></div><div class="ecsp-title" itemprop="name" content="ПУФ"></div><div itemtype="http://schema.org/Offer" itemscope itemprop="offers"> </a> <div class="ecsp-productBrowser-price ecsp-price" itemprop="price" content="1"> <div itemprop="priceCurrency" content="RUB"></div></div></div><div customprop="options"></div><div customprop="addtobag"></div></div>`);
-		}
-	});
-	callback(productsVisLayout);
-	$('#t').html(productsVisLayout.join(""));
-}
-
-function productsNotFound(products) {
-		console.log('Обработка запустилась');
-		if (products.length > 0) {
-			console.log('Возвращаем старый массив');
-			return products
-		} else if (products.length == 0) {
-			products.push(`<div class="product__notfound-wrapper"> <p>По данному запросу нет товаров. Мы работаем над этим :)</p></div>`);
-			console.log('Возвращаем массив 404');
-			return products
-		}
+	var productLayout = ['<div id="my-store-35020171"></div><div>'];
+	productLayout.push(`<div id="my-store-35020171"></div><div><script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?35020171&data_platform=code&data_date=2020-08-18" charset="utf-8">${"</sc"+"ript>"}<script type="text/javascript">window.ec=window.ec||{}; window.ec.storefront=window.ec.storefront||{}; window.ec.storefront.enable_navigation=true;window.ec.storefront.product_details_layout="TWO_COLUMNS_SIDEBAR_ON_THE_RIGHT";window.ec.storefront.product_details_gallery_layout="SINGLE_IMAGE";window.ec.storefront.product_details_two_columns_with_right_sidebar_show_product_description_on_sidebar=true;window.ec.storefront.product_details_two_columns_with_left_sidebar_show_product_description_on_sidebar=false;window.ec.storefront.product_details_show_product_name=true;window.ec.storefront.product_details_show_breadcrumbs=false;window.ec.storefront.product_details_show_product_sku=true;window.ec.storefront.product_details_show_product_price=true;window.ec.storefront.product_details_show_in_stock_label=false;window.ec.storefront.product_details_show_number_of_items_in_stock=false;window.ec.storefront.product_details_show_qty=false;window.ec.storefront.product_details_show_wholesale_prices=false;window.ec.storefront.product_details_show_product_options=true;window.ec.storefront.product_details_show_product_description=true;window.ec.storefront.product_details_show_share_buttons=true;window.ec.storefront.product_details_position_product_name=100;window.ec.storefront.product_details_position_breadcrumbs=200;window.ec.storefront.product_details_position_product_sku=300;window.ec.storefront.product_details_position_product_price=400;window.ec.storefront.product_details_position_product_options=500;window.ec.storefront.product_details_position_buy_button=600;window.ec.storefront.product_details_position_wholesale_prices=700;window.ec.storefront.product_details_position_product_description=800;window.ec.storefront.product_details_position_share_buttons=900;xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","defaultProductId=${product_id}","id=my-store-35020171");${"</sc"+"ript>"}</div>`);
+	$('#t').html(productLayout.join(""));
 }
--- main.js
+++ main.js
@@ -1,28 +1,6 @@
-var products = [
-	{
-		id: '227713178',
-		category: 'Мягкая мебель',
-		vis: 'true',
-	},
-	{
-		id: '227713175',
-		category: 'Корпусная мебель',
-		vis: 'false',
-	},
-	{
-		id: '227713176',
-		category: 'Мягкая мебель',
-		vis: 'false',
-	},
-	{
-		id: '227713180',
-		category: 'Мягкая мебель',
-		vis: 'true',
-	}
-];
-
-var categories = ['Мягкая мебель', 'Корпусная мебель', 'Сантехника, мебель для санузла', 'Освещение, электрика', 'Техника, оборудование', 'Декор', 'Растения'];
-
+//api variables
+var all_products_url = 'https://app.ecwid.com/api/v3/35020171/products?lang=ru&token=public_uwAb5iCSEMGGH2eD1wgQ3NNGuf9E44Xd';
+var all_categories_url = 'https://app.ecwid.com/api/v3/35020171/categories?hidden_categories=true&lang=ru&token=public_uwAb5iCSEMGGH2eD1wgQ3NNGuf9E44Xd';
 var visOptions = ['Все', 'без визуализации', 'с визуализацией'];
 
 $(document).ready(function() {
@@ -35,52 +13,127 @@ $(document).ready(function() {
 	var productInfo = url.get(productParam);
 	var visOption = url.get(visParam);
 	if ((currentUrl.indexOf(filterParam) == -1) && (currentUrl.indexOf(productParam) == -1) && (currentUrl.indexOf(visParam) == -1)) {
-		printProducts();
-		printCategories();
+		$.get(all_products_url, function(data) {
+		printProducts(data, renderProducts.bind(this));
+		});
+		$.get(all_categories_url, function(data) {
+		printCategories(data, renderCategories.bind(this));
+		});
 		printSortVis();
 		setValueVisOption();
 	} else if ((currentUrl.indexOf(filterParam) != -1) && (currentUrl.indexOf(visParam) == -1)) {
 		scrolldiv('shop-body');
-		printProductsFromCategories(catFilter, productsNotFound.bind(this));
-		printCategories();
+		$.get(`https://app.ecwid.com/api/v3/35020171/products?keyword="${catFilter}"&lang=ru&token=public_uwAb5iCSEMGGH2eD1wgQ3NNGuf9E44Xd`, function(data) {
+		printProducts(data, renderProducts.bind(this));
+		});
+		$.get(all_categories_url, function(data) {
+		printCategories(data, renderCategories.bind(this));
+		});
 		printSortVis();
 		setValueVisOption();
 	} else if ((currentUrl.indexOf(filterParam) != -1) && (currentUrl.indexOf(visParam) != -1)) {
 		scrolldiv('shop-body');
-		printProductsVisFromCategories(visOption, catFilter, productsNotFound.bind(this));
-		printCategories();
+		$.get(`https://app.ecwid.com/api/v3/35020171/products?keyword="${catFilter}"&lang=ru&token=public_uwAb5iCSEMGGH2eD1wgQ3NNGuf9E44Xd`, function(data) {
+		printProductsVis(data, visOption, renderProducts.bind(this));
+		});
+		$.get(all_categories_url, function(data) {
+		printCategories(data, renderCategories.bind(this));
+		});
 		printSortVis();
 		setValueVisOption();
 	} else if ((currentUrl.indexOf(filterParam) == -1) && (currentUrl.indexOf(visParam) != -1)) {
 		scrolldiv('shop-body');
-		printProductsVis(visOption, productsNotFound.bind(this));
-		printCategories();
+
+		$.get(all_products_url, function(data) {
+		printProductsVis(data, visOption, renderProducts.bind(this));
+		});
+
+		$.get(all_categories_url, function(data) {
+		printCategories(data, renderCategories.bind(this));
+		});
 		printSortVis();
 		setValueVisOption();
 	} else if (currentUrl.indexOf(productParam) != -1) {
 		scrolldiv('shop-body');
 		printProductInfo(productInfo);
-		printCategories();
+		$.get(all_categories_url, function(data) {
+		printCategories(data, renderCategories.bind(this));
+		});
 	}
 });
 
-function printProducts() {
+function printProducts(data, callback) {
 	var productsLayout = [];
-	products.forEach(function (entry) {
-		let id = entry.id;
+	data.items.forEach(function(item) {
+		let id = item.id;
 		productsLayout.push(`<div class="item ecsp ecsp-SingleProduct-v2 ecsp-SingleProduct-v2-bordered ecsp-Product ec-Product-${id}" itemscope itemtype="http://schema.org/Product" data-single-product-id="${id}"> <a onclick="putProductId('${id}')"> <div itemprop="image"></div><div class="ecsp-title" itemprop="name" content="ПУФ"></div><div itemtype="http://schema.org/Offer" itemscope itemprop="offers"> </a> <div class="ecsp-productBrowser-price ecsp-price" itemprop="price" content="1"> <div itemprop="priceCurrency" content="RUB"></div></div></div><div customprop="options"></div><div customprop="addtobag"></div></div>`);
 	});
-	// ProductsNotFound(productsLayout);
-	$('#t').html(productsLayout.join(""));
+	callback(productsLayout);
 }
 
-function printCategories() {
+function printProductsVis(data, isVis, callback) {
+	console.log(isVis);
+	var productsVisLayout = [];
+	if (isVis == 'true') {
+		data.items.forEach(function(item) {
+			console.log(item.attributes);
+			if (item.attributes) {
+				item.attributes.forEach(function(attr) {
+					if (attr.name == 'Визуализация') {
+						console.log('идём по Визуализация');
+						if (attr.value == 'true') {
+							let id = item.id;
+							productsVisLayout.push(`<div class="item ecsp ecsp-SingleProduct-v2 ecsp-SingleProduct-v2-bordered ecsp-Product ec-Product-${id}" itemscope itemtype="http://schema.org/Product" data-single-product-id="${id}"> <a onclick="putProductId('${id}')"> <div itemprop="image"></div><div class="ecsp-title" itemprop="name" content="ПУФ"></div><div itemtype="http://schema.org/Offer" itemscope itemprop="offers"> </a> <div class="ecsp-productBrowser-price ecsp-price" itemprop="price" content="1"> <div itemprop="priceCurrency" content="RUB"></div></div></div><div customprop="options"></div><div customprop="addtobag"></div></div>`);
+						}
+					}
+				})
+			}
+		});
+	} else if (isVis == 'false') {
+		data.items.forEach(function(item) {
+			console.log(item.attributes);
+			if (item.attributes) {
+				item.attributes.forEach(function(attr) {
+					if (attr.name == 'Визуализация') {
+						console.log('идём по не визуализация');
+						if (attr.value != 'true') {
+							let id = item.id;
+							productsVisLayout.push(`<div class="item ecsp ecsp-SingleProduct-v2 ecsp-SingleProduct-v2-bordered ecsp-Product ec-Product-${id}" itemscope itemtype="http://schema.org/Product" data-single-product-id="${id}"> <a onclick="putProductId('${id}')"> <div itemprop="image"></div><div class="ecsp-title" itemprop="name" content="ПУФ"></div><div itemtype="http://schema.org/Offer" itemscope itemprop="offers"> </a> <div class="ecsp-productBrowser-price ecsp-price" itemprop="price" content="1"> <div itemprop="priceCurrency" content="RUB"></div></div></div><div customprop="options"></div><div customprop="addtobag"></div></div>`);
+						}
+					} 
+				})
+			} else {
+				let id = item.id;
+				productsVisLayout.push(`<div class="item ecsp ecsp-SingleProduct-v2 ecsp-SingleProduct-v2-bordered ecsp-Product ec-Product-${id}" itemscope itemtype="http://schema.org/Product" data-single-product-id="${id}"> <a onclick="putProductId('${id}')"> <div itemprop="image"></div><div class="ecsp-title" itemprop="name" content="ПУФ"></div><div itemtype="http://schema.org/Offer" itemscope itemprop="offers"> </a> <div class="ecsp-productBrowser-price ecsp-price" itemprop="price" content="1"> <div itemprop="priceCurrency" content="RUB"></div></div></div><div customprop="options"></div><div customprop="addtobag"></div></div>`);
+			}
+		});
+	}
+	callback(productsVisLayout);
+}
+
+function renderProducts(layout) {
+	if (layout.length > 0) {
+			console.log('Возвращаем старый массив');
+	} else if (layout.length == 0) {
+			layout.push(`<div class="product__notfound-wrapper"> <p>По данному запросу нет товаров. Мы работаем над этим :)</p></div>`);
+			console.log('Возвращаем массив 404');
+	}
+	$('#t').html(layout.join(""));
+	console.log($('#t'));
+}
+
+function printCategories(data, callback) {
+	console.log(data);
 	var categoriesLayout = [];
-	categories.forEach(function(entry) {
-		let name = entry;
+	data.items.forEach(function(item) {
+		let name = item.name;
 		categoriesLayout.push(`<li class="category__item" onclick="putParamsUrl('${name}')">${name}</li>`);
-		$('#categories').html(categoriesLayout.join(""));
 	});
+	callback(categoriesLayout);
+}
+
+function renderCategories(layout) {
+	$('#categories').html(layout.join(""));
 }
 
 function scrolldiv(str) { 
