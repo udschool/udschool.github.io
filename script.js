23e50646d23c7988b3f4dceda4cee420cd4ee3ec
sk8beemo <skateor2007@gmail.com>
2020-08-27 15:43:11 +0300

    email add

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+document.addEventListener('DOMContentLoaded', function() {
+   printCustomerEmail();
+}, false);
+function printCustomerEmail() {
+	Ecwid.OnSetProfile.add(function(customer) {
+	  $('.tn-elem__2233100061598348258199').children(".tn-atom").html(Array.from(customer ? ('Личный кабинет('+customer.email+')') : "Войти").join(""))
+	});
+}
