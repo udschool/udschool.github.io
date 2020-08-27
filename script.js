1c9ec4e69a65a83fd295d50c3121d249e54ee8ae
sk8beemo <skateor2007@gmail.com>
2020-08-27 15:49:19 +0300

    remove email

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-   printCustomerEmail();
-}, false);
-function printCustomerEmail() {
-	Ecwid.OnSetProfile.add(function(customer) {
-	  $('.tn-elem__2233100061598348258199').children(".tn-atom").html(Array.from(customer ? ('Личный кабинет('+customer.email+')') : "Войти").join(""))
-	});
-}
+// document.addEventListener('DOMContentLoaded', function() {
+//    printCustomerEmail();
+// }, false);
+// function printCustomerEmail() {
+// 	Ecwid.OnSetProfile.add(function(customer) {
+// 	  $('.tn-elem__2233100061598348258199').children(".tn-atom").html(Array.from(customer ? ('Личный кабинет('+customer.email+')') : "Войти").join(""))
+// 	});
+// }
