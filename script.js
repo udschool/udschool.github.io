e7c23c7faab2063ff97c291dd344f01e8eef1d53
sk8beemo <skateor2007@gmail.com>
2020-08-31 12:09:23 +0300

    js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 Ecwid.OnPageLoaded.add(function(page) {
 	console.log(page.type);
+	if (page.type == "ACCOUNT_SETTINGS") {
+		Ecwid.OnSetProfile.add(function(customer) {
+			$.get(`https://app.ecwid.com/api/v3/35020171/orders?customer=${customer.email}&token=secret_dYSNe7rT6hY73H8HhAZeJNQMdmXxifLz`, function(data) {
+				printProductsInCart(data, renderProducts.bind(this));
+			});
+		})
+  	}
 });
+
+function printProductsInCart (orders, callback) {
+	var productsLayout = [];
+	orders.items.forEach(function(entry) {
+		entry.items.forEach(function(entry) {
+			console.log(entry);
+		});
+	});
+	callback(productsLayout);
+}
+function renderProducts(layout) {
+	if (layout.length > 0) {
+			// console.log('Возвращаем старый массив');
+	} else if (layout.length == 0) {
+			layout.push(`<div class="product__notfound-wrapper"> <p>По данному запросу нет товаров. Мы работаем над этим :)</p></div>`);
+			// console.log('Возвращаем массив 404');
+	}
+	// $('#t').html(layout.join(""));
+	// console.log(layout);
+}
