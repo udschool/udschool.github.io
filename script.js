29a4a2f019e445f938eb14c19c77e4a36a842f4c
sk8beemo <skateor2007@gmail.com>
2020-09-04 16:28:15 +0300

    add text below filter

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ Ecwid.OnPageLoaded.add(function(page) {
 				hiddenProductsFromStorefront(data);
 			});
 		});
-  		setTimeout(addTextBelowFilter('.ec-filter--attribute-04140435043a043e0440-0443043d043804320435044004410430043b044c043d044b0439'), 2000);
+  		setTimeout(addTextBelowFilter('ec-filter--attribute-041c043e04340435043b0438-0434043b044f-043604380432043e0442043d044b0445'), 2000);
   		// printFilterMenu();
   	} else if (page.type == "PRODUCT") {
   		Ecwid.OnSetProfile.add(function(customer) {
@@ -74,7 +74,7 @@ function openProductDetail(productId) {
 function addTextBelowFilter(element){
 	let div = document.createElement('div');
 	div.className = 'ec-filter__alert'
-	div.innerHTML = '<strong>Всем привет!</strong>';
+	div.innerHTML = '<strong>Фильтры:</strong>';
 	document.querySelector(element).append(div);
 }
 
